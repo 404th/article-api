845a69e6305bf5d6cef5bc747467a67b72a49a8a
404th <umarov.doniyor.2002@gmail.com>
2021-06-26 21:40:37 +0500

    check is fixed

--- routes/article.js
+++ routes/article.js
@@ -1,21 +1,25 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 const {
-  article_post_create,
-  article_get_get,
-  article_delete_remove,
-  article_get_all,
-  comment_post_create,
-  comment_get_all,
+    article_post_create,
+    article_get_get,
+    article_delete_remove,
+    article_get_all,
+    comment_post_create,
+    comment_get_all,
 } = require("./controllers/cont_article");
 //
 router.post(
-  "/",
-  [
-    check("title", "title should be filled").isString().isLength({ min: 10 }),
-    check("body", "body should be filled").isString().isLength({ min: 10 }),
-  ],
-  article_post_create(validationResult)
+    "/",
+    [
+        check("title", "title should be min 5 chars")
+            .isString()
+            .isLength({ min: 5 }),
+        check("body", "body should be min 10 chars")
+            .isString()
+            .isLength({ min: 10 }),
+    ],
+    article_post_create(validationResult)
 );
 
 router.get("/:id", article_get_get());
@@ -24,13 +28,13 @@ router.get("/", article_get_all());
 
 ////
 router.post(
-  "/comment/:article_id",
-  [
-    check("comment_body", "comment_body should be filled")
-      .isString()
-      .isLength({ min: 10 }),
-  ],
-  comment_post_create(validationResult)
+    "/comment/:article_id",
+    [
+        check("comment_body", "comment_body should be filled")
+            .isString()
+            .isLength({ min: 10 }),
+    ],
+    comment_post_create(validationResult)
 );
 router.get("/comment/:article_id", comment_get_all());
 
--- server.js
+++ server.js
@@ -9,32 +9,32 @@ app.use(cors());
 app.use(express.json());
 
 async function connMB(article_uri) {
-  try {
-    await mongoose.connect(
-      article_uri,
-      {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      },
-      (err) => {
-        if (err) {
-          console.log(err);
-          console.log("error while connecting to DB");
-        } else {
-          console.log("Successfully connected to DB");
-        }
-      }
-    );
-  } catch (err) {
-    return createError(
-      new Error(err),
-      "can't connect to db",
-      "mongo",
-      "server.js"
-    );
-  }
+    try {
+        await mongoose.connect(
+            article_uri,
+            {
+                useCreateIndex: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+            },
+            (err) => {
+                if (err) {
+                    console.log(err);
+                    console.log("error while connecting to DB");
+                } else {
+                    console.log("Successfully connected to DB");
+                }
+            }
+        );
+    } catch (err) {
+        return createError(
+            new Error(err),
+            "can't connect to db",
+            "mongo",
+            "server.js"
+        );
+    }
 }
 
 //start server
